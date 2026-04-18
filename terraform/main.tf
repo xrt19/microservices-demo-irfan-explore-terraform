7d35d85938aa97126145441648c40e09764b4f63
irfan hn <irfanhera17@gmail.com>
2026-04-19 00:58:13 +0700

    fix flag zone to region

--- terraform/main.tf
+++ terraform/main.tf
@@ -96,7 +96,7 @@ module "gcloud" {
   create_cmd_entrypoint = "gcloud"
   # Module does not support explicit dependency
   # Enforce implicit dependency through use of local variable
-  create_cmd_body = "container clusters get-credentials ${local.cluster_name} --zone=${var.region} --project=${var.gcp_project_id}"
+  create_cmd_body = "container clusters get-credentials ${local.cluster_name} --region=${var.region} --project=${var.gcp_project_id}"
 }
 
 # Apply YAML kubernetes-manifest configurations
